cf5ea12cb65f197dee0004e34d9c43e643dc5d68
Zakariaae Belkassem <zakariaebelkassem@gmail.com>
2025-11-05 12:47:45 +0100

    Search insert solution - Problem n 35

---
+++ src/main/java/searchInsert.java
@@ -0,0 +1,18 @@
+public class searchInsert {
+
+        public static  int searchInsert(int[] nums, int target) {
+            int left = 0, right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) return mid;
+                if (nums[mid] < target) left = mid + 1;
+                else right = mid - 1;
+            }
+            return left; // position to insert
+        }
+
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[] {1}, 1));
+    }
+}
