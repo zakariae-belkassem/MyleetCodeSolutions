b4a18d4b9476cce320b8dcdd5bce156576a09196
Zakariaae Belkassem <zakariaebelkassem@gmail.com>
2025-11-07 10:48:17 +0100

    solution - Problem N°125

---
+++ src/main/java/N125.java
@@ -0,0 +1,14 @@
+public class N125   {
+    public static boolean isPalindrome(String s) {
+
+       StringBuilder sb = new StringBuilder();
+        for (int k = 0; k < s.length(); k++) {
+            if(Character.isLetter(s.charAt(k)) || Character.isDigit(s.charAt(k))){
+                sb.append(Character.toLowerCase(s.charAt(k)));
+            }
+        }
+        System.out.println(sb);
+        return sb.toString().contentEquals(sb.reverse());
+    }
+
+}
