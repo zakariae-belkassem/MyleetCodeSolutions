07eb71ec220e05eb2c9360ef13e302bc5c69b0cf
Zakariaae Belkassem <zakariaebelkassem@gmail.com>
2025-11-10 15:10:06 +0100

    solution - Problem N°13

--- src/main/java/N13.java
+++ src/main/java/N13.java
@@ -7,13 +7,9 @@ public class N13 {
                 'I', 1, 'V', 5, 'X', 10,
                 'L', 50, 'C', 100, 'D', 500, 'M', 1000
         );
-
         int result = 0;
-
         for (int i = 0; i < s.length(); i++) {
             int value = map.get(s.charAt(i));
-
-            // Check if the next value is larger (subtractive notation)
             if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
                 result -= value;
             } else {
