c0b80d9afe8221580dd617f1bfc88c57186206f9
Zakariaae Belkassem <zakariaebelkassem@gmail.com>
2025-11-10 15:09:29 +0100

    solution - Problem N°13

---
+++ src/main/java/N13.java
@@ -0,0 +1,29 @@
+import java.util.Map;
+
+public class N13 {
+
+    public  static int romanToInt(String s) {
+        Map<Character, Integer> map = Map.of(
+                'I', 1, 'V', 5, 'X', 10,
+                'L', 50, 'C', 100, 'D', 500, 'M', 1000
+        );
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = map.get(s.charAt(i));
+
+            // Check if the next value is larger (subtractive notation)
+            if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
+                result -= value;
+            } else {
+                result += value;
+            }
+        }
+
+        return result;
+
+    }
+
+
+}
---
+++ src/main/java/NotYet/N3.java
@@ -0,0 +1,23 @@
+package NotYet;
+
+public class N3 {
+
+
+    public static  int lengthOfLongestSubstring(String s) {
+
+        String[] res = s.split("");
+        for (int i = 0; i < res.length; i++) {
+            if (true) {
+                System.out.println("dd");
+            }
+        }
+
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
